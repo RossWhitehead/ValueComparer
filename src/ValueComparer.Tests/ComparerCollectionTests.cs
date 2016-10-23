331676e80c675ce5547dab5564a0dd528cde2ba1
Ross Whitehead <rossshaggywhitehead@hotmail.com>
2016-10-23 08:39:03 +0100

    Separate Collection tests into own class

---
+++ src/ValueComparer.Tests/ComparerCollectionTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Xunit.Extensions;
+
+namespace ValueComparer
+{
+    public class ComparerCollectionTests
+    {
+        public ComparerCollectionTests()
+        {
+        }
+
+        [Theory]
+        [MemberData(("CollectionsData"), DisableDiscoveryEnumeration = true)]
+        public void Compare_ReturnsExpectedResult_ForCollections(object object1, object object2, bool expectedResult)
+        {
+            // Act
+            var actualResult = ValueComparer.Compare(object1, object2);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        public static IEnumerable<object[]> CollectionsData()
+        {
+            return new[]
+            {
+                new object[]
+                {
+                    new int[2] { 1, 2 },
+                    new int[2] { 1, 2 },
+                    true
+                },
+                new object[]
+                {
+                    new int[2] { 1, 2 },
+                    new int[2] { 1, 3 },
+                    false
+                },
+                new object[]
+                {
+                    new int[2] { 1, 2 },
+                    new int[3] { 1, 2, 3 },
+                    false
+                },
+                // The MemberData attribute needs DisableDiscoveryEnumeration = true for the following to work
+                new object[]
+                {
+                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
+                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
+                    true
+                },
+                 new object[]
+                {
+                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
+                    new int[2, 2] { { 1, 6 }, { 3, 4 } },
+                    false
+                }
+            };
+        }
+    }
+}
--- src/ValueComparer.Tests/ComparerTests.cs
+++ src/ValueComparer.Tests/ComparerTests.cs
@@ -70,55 +70,6 @@ namespace ValueComparer
             Assert.Equal(false, actualResult);
         }
 
-        [Theory]
-        [MemberData(("CollectionsData"), DisableDiscoveryEnumeration = true)]
-        public void Compare_ReturnsExpectedResult_ForCollections(object object1, object object2, bool expectedResult)
-        {
-            // Act
-            var actualResult = ValueComparer.Compare(object1, object2);
-
-            // Assert
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        public static IEnumerable<object[]> CollectionsData()
-        {
-            return new[]
-            {
-                new object[]
-                {
-                    new int[2] { 1, 2 },
-                    new int[2] { 1, 2 },
-                    true
-                },
-                new object[]
-                {
-                    new int[2] { 1, 2 },
-                    new int[2] { 1, 3 },
-                    false
-                },
-                new object[]
-                {
-                    new int[2] { 1, 2 },
-                    new int[3] { 1, 2, 3 },
-                    false
-                },
-                // The MemberData attribute needs DisableDiscoveryEnumeration = true for the following to work
-                new object[]
-                {
-                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
-                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
-                    true
-                },
-                 new object[]
-                {
-                    new int[2, 2] { { 1, 2 }, { 3, 4 } },
-                    new int[2, 2] { { 1, 6 }, { 3, 4 } },
-                    false
-                }
-            };
-        }
-
         private class Fake
         {
             public int Prop1 { get; set; }
